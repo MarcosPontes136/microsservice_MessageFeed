e9e58af04656f5fa8a149f970e64f87c09b49a3b
MarcosPontes136 <zeuscod@outlook.com>
2024-05-18 17:35:07 -0300

    hotfix/Log para envio de mensagens

--- src/main/java/com/microsservice_feedMessage/service/MessageService.java
+++ src/main/java/com/microsservice_feedMessage/service/MessageService.java
@@ -3,6 +3,8 @@ package com.microsservice_feedMessage.service;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,7 @@ import com.microsservice_feedMessage.dto.MessageDTO;
 import com.microsservice_feedMessage.enums.StatusMessage;
 import com.microsservice_feedMessage.models.MessageModel;
 import com.microsservice_feedMessage.repositories.MessageRepository;
+import com.microsservico_email.Service.EmailService;
 import com.rabbitmq.client.AMQP.Basic.Get;
 
 import jakarta.mail.MessagingException;
@@ -19,17 +22,22 @@ public class MessageService {
 
 	@Autowired
 	private MessageRepository messageRepository;
+	
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
     
 	public MessageModel saveMessage(MessageModel messageModel)throws UnsupportedEncodingException, MessagingException {
 		
 		try {
-			
+			logger.info("Tentando montar nome do remetente {}", messageModel.getNome());
+			logger.info("Tentando montar mensagem {}", messageModel.getMensagem());
 	        MessageDTO messageDTO = new MessageDTO();
 	        messageDTO.setNome(messageModel.getNome());
 	        messageDTO.setMensagem(messageModel.getMensagem());
 	        messageModel.setStatusMessage(StatusMessage.SENT);
+	        logger.info("Mensagem montada com sucesso! {}", messageModel.getStatusMessage());
 		} catch (Exception e) {
 			messageModel.setStatusMessage(StatusMessage.ERROR);
+			logger.info("Erro ao montar mensagem! {}", messageModel.getStatusMessage());
 		}
 		
 		return messageRepository.save(messageModel);
